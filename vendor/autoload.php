d32cdc52ca56889df1569c8f566b15f059697982
leprosy <leprosy@leprosy-laptop>
2018-08-16 12:00:20 +0300

    Added phpcs

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,6 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-require_once __DIR__ . '/../lib/MemCache.php';
-
 return ComposerAutoloaderInit7de5de47f3e4b684f5f0ff56c3da804f::getLoader();
